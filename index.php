731b6f977ed6f3eb87fe8f4f9d385e91804dc6c6
Boubacar <boubacardrame9@gmail.com>
2022-02-09 15:13:48 +0100

    challenge6

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+    function writeSecretSentence($firstWord, $secondWord) {
+        return "$firstWord s’incline face à $secondWord";
+    }
+
+   echo writeSecretSentence('lune', 'feu');
+?>
